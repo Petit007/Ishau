fa3378810cd792cdb3957dd0cfd0168d834e8145
Ishau Oyeti <ishau@Ishaus-MacBook-Pro.local>
2020-03-24 21:00:05 -0500

    updated index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var name = "Ishau Oyeti";
+var courses = ["html", "css", "javascript"];
+console.log(name);
+for(i=0;i<3;i++){
+    console.log(courses[i]);
+}
+for(i=1;i<=200;i++){
+    if(i%2!=0){
+    console.log([i]);
+}}
